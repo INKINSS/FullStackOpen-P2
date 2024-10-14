0102fcde96a863f8446a353998b1872d1b9bcdeb
samito <martinsamuelpp@gmail.com>
2024-10-13 19:31:34 -0600

    2.1: Información del curso paso 6 complete

---
+++ src/components/Course.jsx
@@ -0,0 +1,11 @@
+const Course = ({ courses }) => {
+  return (
+    <main>
+        { courses.parts.map((course) => (
+            <p style={{margin:'10px 0'}} key={course.id}>{course.name} <span>{course.exercises}</span></p>
+        )) }
+    </main>
+  );
+};
+
+export default Course;
